41979cfa15fd937001b50c16c1172dff1ff70fcc
edvegauiowa16 <levega@yahoo.com>
2016-11-10 15:23:27 -0600

    created interface for Belt

--- src/Belt/Belt.java
+++ src/Belt/Belt.java
@@ -1,47 +1,14 @@
 package Belt;
 
-import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Observer;
-
 /**
- * Created by Eduardo   on 11/3/16.
- * Group B4
+ * Created by Eduardo on 11/10/2016.
  */
-///needtorun = true  robot parked at picker station
+public interface Belt {
+    /**
+     * created for interface
+     * method to start the packing of the order given
+     * @param orderID : id of the order or identifier
+     */
+    public void pack (Integer orderID );
 
-public class Belt implements Observer
-{
-	public ArrayList<Integer> shippedItemIDList;
-	public int x_loc_pick;
-	public int y_loc_pick;
-	
-	//receives item from Robot, and sends to belt to reach packer
-	public void pack(Integer OrderID)  //receive from picker and send to packer
-	{
-		ship(OrderID);
-	}
-	
-	//ships out of warehouse   and store in list of shipped orders
-	public void ship(Integer orderID)
-	{
-		shippedItemIDList.add(orderID);
-	}
-	
-	public void onSite_Pick(int x, int y)
-	{
-		Integer orderID = 0;
-		if(x_loc_pick ==  x && y_loc_pick ==  y)
-		{
-			pack(orderID);
-		}
-	}
-	
-	@Override
-    public void update(Observable o, Object arg)
-	{
-
-
-	}
-	
-}	
+}
---
+++ src/Belt/BeltImpl.java
@@ -0,0 +1,47 @@
+package Belt;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by Eduardo   on 11/3/16.
+ * Group B4
+ */
+///needtorun = true  robot parked at picker station
+
+public class BeltImpl implements Observer, Belt
+{
+	public ArrayList<Integer> shippedItemIDList;
+	public int x_loc_pick;
+	public int y_loc_pick;
+	
+	//receives item from Robot, and sends to belt to reach packer
+	public void pack(Integer OrderID)  //receive from picker and send to packer
+	{
+		ship(OrderID);
+	}
+	
+	//ships out of warehouse   and store in list of shipped orders
+	private void ship(Integer orderID)
+	{
+		shippedItemIDList.add(orderID);
+	}
+	
+	public void onSite_Pick(int x, int y)
+	{
+		Integer orderID = 0;
+		if(x_loc_pick ==  x && y_loc_pick ==  y)
+		{
+			pack(orderID);
+		}
+	}
+	
+	@Override
+    public void update(Observable o, Object arg)
+	{
+
+
+	}
+	
+}	

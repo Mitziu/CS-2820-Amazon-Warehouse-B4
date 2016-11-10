70bff103b51abf12dcaf6901e2ca46bc43f78270
mattmccan <matthew-mccan@uiowa.edu>
2016-11-10 16:51:02 -0600

    Create Interface For Floor to Implement - fixed a few things

--- src/Floor/FloorPositions.java
+++ src/Floor/FloorPositions.java
@@ -4,14 +4,15 @@ import java.util.Map;
 
 /**
  * Created by Matt on 11/10/2016.
+ * @author Matt
  * Please implement this so that the visualizer can see your stuff.
  */
 public interface FloorPositions {
 
     /**
      *
-     * @return: A map | Keys are objects on the floor--perhaps object Id | Values are a data structure |
-     *  Containing positions, type of object | Later, we can add more info, but for now that should be enough
+     * @return A map | Keys are objects on the floor--perhaps object Id | Values are a data structure |
+     * Containing positions, type of object | Later, we can add more info, but for now that should be enough
      *
      *  Feel free to change the Key/Value to suit your needs
      */

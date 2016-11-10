66ac34e834c751cdb601d4717925eb0559236f59
mattmccan <matthew-mccan@uiowa.edu>
2016-11-10 16:43:39 -0600

    Create Interface For Floor to Implement

--- src/Floor/FloorPositions.java
+++ src/Floor/FloorPositions.java
@@ -1,4 +1,6 @@
 package Floor;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Matt on 11/10/2016.
@@ -6,5 +8,13 @@ package Floor;
  */
 public interface FloorPositions {
 
+    /**
+     *
+     * @return: A map | Keys are objects on the floor--perhaps object Id | Values are a data structure |
+     *  Containing positions, type of object | Later, we can add more info, but for now that should be enough
+     *
+     *  Feel free to change the Key/Value to suit your needs
+     */
+    Map<Integer, List<Object>> getPositions();
 
 }

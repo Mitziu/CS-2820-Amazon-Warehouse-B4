747cfc815703b782002f77261624b8edcbd02277
maximightiest <maxwell-riley@uiowa.edu>
2016-11-11 10:12:24 -0600

    Created interface for Orders
    Just some basic functions that will be used to send data to other parts of the code considering orders

---
+++ src/Ordering/OrderInterface.java
@@ -0,0 +1,28 @@
+import java.util.*
+
+/**
+*
+*
+*@author Max Riley
+*
+*/
+
+public interface OrderInterface {
+    //OrderItem is the class we have/will make for an Item
+    //Used to tell robot to get item and put it on belt
+    public void getItem(OrderItem item, Shelf shelfLocation);
+    
+    
+    //Used to send the belt a notification that a complete order is on the belt and ready to move
+    public void startBelt();
+    
+    //Get the shelf on which the item is on
+    public void getItemLocation(OrderItem item);
+    
+    
+    //Check to see if item is in stock
+    public bool isItemInStock(OrderItem item);
+    
+    
+
+

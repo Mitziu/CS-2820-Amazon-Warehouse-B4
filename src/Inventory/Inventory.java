1c5f97234536ce37442464ff495c03da3031a969
will2842 <william-anderson@uiowa.edu>
2016-11-11 10:01:11 -0600

    Filled out the inventory Class more.
    Added several methods for adding items and to get information about
    them.

--- src/Inventory/Inventory.java
+++ src/Inventory/Inventory.java
@@ -1,18 +1,29 @@
-package Inventory;
+package inventory
 
 import java.util.*;
+
+import Inventory.Item_Details;
 public class Inventory {
 	
-	private class Item_Details{
+	class Item_Details{
+		HashMap <String, Integer> Containers;
 		int Item_ID;
 		String Item_Name;
 		int Item_Quantity;
+		
 		Item_Details(int Item_ID, String Item_Name, int Item_Quantity){
 			this.Item_ID = Item_ID;
 			this.Item_Name = Item_Name;
 			this.Item_Quantity = Item_Quantity;
+
+		}
+		
+		public void Qty_Update(){
+			for (int value : Containers.values()) {
+			    Item_Quantity += value;
 		}
 	}
+	
 	public HashMap <Integer, Item_Details> Main_Inventory = new HashMap<Integer, Item_Details>();
 	public HashMap <String, Integer> Converter = new HashMap<String, Integer>();
 	
@@ -28,6 +39,17 @@ public class Inventory {
 		return Converter.get(name);
 	}
 	
+	public LinkedList<String> Contained_In(int ID){
+		Set<String> Tempset = Main_Inventory.get(ID).Containers.keySet();
+		LinkedList<String> RetList = new LinkedList<String>(Tempset); 
+		return RetList;
+	}
+	
+	public void Add_Inventory(int ID, String Name, int Qty){
+		Item_Details New_Item = new Item_Details(ID, Name, Qty);
+		Main_Inventory.put(New_Item.Item_ID, New_Item);
+	}
+	
+	
 }
-
 

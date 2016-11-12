4498bef0a0a3e6f3bdb5d18c13a360ba74e3c779
mynameislich <zuoyuan-zhao@uiowa.edu>
2016-11-12 14:48:44 -0600

    fixed Shelf.id and implemented getAllPositions()

--- src/Floor/Setup.java
+++ src/Floor/Setup.java
@@ -29,13 +29,28 @@ public class Setup implements FloorPositions {
 		for(int i = 0; i< index.size();i++){
 			for(int j = 0; j<7;j++){
 				int y = index.get(i);
-				Shelf temp = new Shelf(4+j,y,i);
+                                                                                String s = String.valueOf( 4+j )+ String.valueOf(i);
+				Shelf temp = new Shelf(4+j,y,Integer.valueOf(s));
 				SA.add(temp);
 			}
 		}
 	}
-	public HashMap<String, ArrayList<Point>> getAllPositions(){
-		HashMap<String, ArrayList<Point>> m_map = new HashMap<String, ArrayList<Point>>();
+	public HashMap<String, Point> getAllPositions(){
+		HashMap<String, Point> m_map = new HashMap<String, Point>();
+                                        Point f = new Point(f1.getx(),f1.gety());
+                                        m_map.put("floor", f);
+                                        f = new Point(picker.getx(),picker.gety());
+                                        m_map.put("picker", f) ;
+                                        f = new Point(packager.getx(),packager.gety());
+                                        m_map.put("packager", f) ;
+                                        f = new Point(charge.getx(),charge.gety());
+                                        m_map.put("charge", f) ;
+                                        f = new Point(f_belt.getx(),f_belt.gety());
+                                        m_map.put("belt", f) ;
+                                        for(int i = 0;i<SA.size();i++){
+                                                  f = new Point(SA.get(i).getx(),SA.get(i).gety());
+                                                  m_map.put(SA.get(i).name, f) ;
+                                         }
 		return m_map;
 	}
 }

7c8d38ccbf0c395588cc8833a400cfceb43cb687
Mitziu <mitziuecheverria@gmail.com>
2016-11-17 17:14:02 -0600

    Created Picker interface

---
+++ src/Belt/Picker.java
@@ -0,0 +1,26 @@
+package Belt;
+
+import Floor.Shelf;
+import Ordering.Order;
+
+/**
+ * Created by Mitziu on 11/17/16.
+ * @author Mitziu
+ */
+public interface Picker {
+
+    /**
+     * Informs the Picker of a new order that will need to be fulfilled
+     * @param newOrder Order to be fulfilled
+     */
+    public void newOrder (Order newOrder);
+
+
+    /**
+     * Informs the Picker that a new shelf has arrived
+     * @param shelf Shelf has arrived
+     */
+    public void shelfArrived (Shelf shelf);
+    //TODO: Need to create a single shelf class
+
+}

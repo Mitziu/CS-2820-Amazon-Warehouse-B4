2f540dc23a712c565c388b4335f05501990c95b9
maximightiest <maxwell-riley@uiowa.edu>
2016-11-29 09:50:31 -0600

    Added a test Order file, not complete yet

---
+++ tst/OrderTest.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Max on 11/29/2016.
+ * @author Max Riley
+ *
+ *
+ */
+
+import Belt.Belt;
+import Ordering.OrderingSystem;
+import Ordering.Order;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import Belt.BeltImpl;
+import Inventory.Inventory;
+import RobotScheduler.RobotScheduler;
+import Belt.Picker;
+
+public class OrderTest {
+
+    Inventory testInvent = new Inventory();
+    RobotScheduler testRobot = new RobotScheduler("TestRobot");
+
+    @Test
+    public void CreateOrder(){
+        OrderingSystem testOrderSystem = new OrderingSystem();
+
+
+
+    }
+
+}

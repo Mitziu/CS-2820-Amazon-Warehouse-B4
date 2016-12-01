da76102f8108d3d0741f81d8012ce3eff66757e7
Virocider <yue-lei@uiowa.edu>
2016-12-01 14:04:44 -0600

    Update Robots.java
    Improve readability.

--- src/Floor/Robots.java
+++ src/Floor/Robots.java
@@ -2,24 +2,27 @@ package Floor;
 
 import java.util.ArrayList;
 public class Robots implements Thing{ //contains robots and shelves
-	public  int w= 1;
-	public  int h=1;
-	public  int id;
-	public  Shelf m_shelf;
-                     public boolean Carried= false;
-                     public  int x_loc;
-	public  int y_loc;
-	public  int width;
-	public  int length;
-	public  boolean moveable; 
-                     public  boolean needCharge = false; 
-                     int priority ;
+	public int w = 1;
+	public int h = 1;
+	public int id;
+	public Shelf m_shelf;
+        public boolean Carried = false;
+	
+        public int x_loc;
+	public int y_loc;
+	public int width;
+	public int length;
+	
+	public boolean moveable; 
+        public boolean needCharge = false; 
+        public int priority ;
 	public double battery = 1.00;
-                     public ArrayList<Point> commands;
-                     public Point OrginOfShelf;
-                     public Setup m_setup;
+	
+        public ArrayList<Point> commands;
+        public Point OrginOfShelf;
+        public Setup m_setup;
 	public Robots(int x, int y, int s,Setup input){
-                                           x_loc= x; 
+		x_loc= x; 
 		y_loc= y ;
 		width= w;
 		length= h;
@@ -30,7 +33,7 @@ public class Robots implements Thing{ //contains robots and shelves
 	}
 	
         @Override
-                    public int getx(){ //x coordinate
+        public int getx(){ //x coordinate
 		return x_loc;
 	}
         @Override
@@ -50,52 +53,51 @@ public class Robots implements Thing{ //contains robots and shelves
 		moveable = true;
 	}
         @Override
-          public void move(int x, int y){
-                     if(!needCharge){
-                                if(this.battery>0.1){
+        public void move(int x, int y){
+		if(!needCharge){
+			if(this.battery>0.1){
+				if(moveable){
+					x_loc = x;
+					y_loc = y;
+				}
+				commands.remove(0);
+                                this.battery = this.battery - 0.0001;
+			}else{
+				needCharge= true;
+				Charging();
+			}
+		}else{
+			LowPowerMode(x,y);
+		}
+	}
+	public void LowPowerMode(int x, int y){
 		if(moveable){
 			x_loc = x;
 			y_loc = y;
+			commands.remove(0);
 		}
-                                           commands.remove(0);
-                                           this.battery = this.battery - 0.0001;
-                                           
-                                }else{
-                                            needCharge= true;
-                                            Charging();
-                                }
-	}else{
-                                LowPowerMode(x,y);
-                     }
-           }
-                    public void LowPowerMode(int x, int y){
-                                if(moveable){
-                                    x_loc = x;
-                                    y_loc = y;
-                                    commands.remove(0);
-                                }
-                    }
-                    public Point getLoc(){
+	}
+        public Point getLoc(){
 		return new Point(x_loc,y_loc);
 	}
-                    public boolean HasShelf(){
-                                return this.Carried;
-                    }
-                    public void CarryUp(Shelf input){
-                                this.Carried = true;
-                                m_shelf = input;
-                    }
-                    public void Charging(){
-                                commands.clear();
-                                if(!this.HasShelf()){
-                                            commands = m_setup.RouteFinding(m_setup.getChargingStation().getLoc(),this.getLoc());
-                                }else{
-                                             commands = m_setup.RouteFinding(this.OrginOfShelf,this.getLoc());
-                                             ArrayList<Point>  temp = new  ArrayList<Point>();
-                                             temp =   m_setup.RouteFinding(m_setup.getChargingStation().getLoc(),this.getLoc());
-                                             for(int i =0;i<temp.size();i++){
-                                                     commands.add(temp.get(i));
-                                             }
-                                }           
-                    }
+        public boolean HasShelf(){
+		return this.Carried;
+        }
+        public void CarryUp(Shelf input){
+		this.Carried = true;
+                m_shelf = input;
+	}
+        public void Charging(){
+		commands.clear();
+		if(!this.HasShelf()){
+			commands = m_setup.RouteFinding(m_setup.getChargingStation().getLoc(), this.getLoc());
+		}else{
+			commands = m_setup.RouteFinding(this.OrginOfShelf, this.getLoc());
+			ArrayList<Point> temp = new ArrayList<Point>();
+			temp = m_setup.RouteFinding(m_setup.getChargingStation().getLoc(), this.getLoc());
+                        for(int i = 0; i < temp.size(); i++){
+				commands.add(temp.get(i));
+			}
+		}           
+        }
 }

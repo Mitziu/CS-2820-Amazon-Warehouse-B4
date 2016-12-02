895b9072137d27f634011a111ee4f5e881cef7f2
Mitziu <mitziuecheverria@gmail.com>
2016-12-02 17:51:48 -0600

    Added Production class which will run the simulation for the entire warehouse

---
+++ src/Production.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Mitziu on 12/2/16.
+ * Class to run simulation of the entire warehouse
+ */
+public class Production {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
